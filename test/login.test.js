372954988700e7672435c731a7de439b0b6cfd70
Ricky Vang <vangcheemeng@yahoo.com>
2023-06-07 16:53:46 -0600

    W08 Automated API

---
+++ test/login.test.js
@@ -0,0 +1,52 @@
+// const fetch = require('node-fetch');
+
+// it("Should get a login token", async () => {
+
+//     let token = "";
+//     const options = {
+//         method: "POST",
+//         headers:{
+//             "Content-Type": "application/json"
+//         },
+//         body:JSON.stringify({
+//             "userName": "vangcheemeng@yahoo.com",
+//             "password": "Cheemengvang1234!"
+//         })
+//     }
+
+//     const response = await fetch('https://dev.stedi.me/login',options);
+
+//     token = await response.text();
+//     console.log("Token " + token);
+//     const status = response.status;
+
+//     expect(status).toBe(200);
+//     expect(token.length).toBe(36);
+// })
+
+const fetch = require("node-fetch2");
+
+it("Should get a login token",async ()=>{
+
+    let token = "";//this is an emptry string
+    const options = {
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+            "userName":"vangcheemeng@yahoo.com",
+            "password":"Cheemengvang1234!"
+        })
+    }
+
+    const response = await fetch('https://dev.stedi.me/login',options);
+
+    token = await response.text();
+    console.log("Token "+token);
+    const status = response.status;
+
+    expect(status).toBe(200);
+    expect(token.length).toBe(36);
+
+})
